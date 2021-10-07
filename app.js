919999dba276cd831ca46c19552e59619c541841
kwonsean <gownn12@naver.com>
2021-10-07 18:18:32 +0900

    feat: Route & Render for /chat

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.use("/public", express.static(__dirname + "/public"));
 
 const indexRouter = require('./routes/index');
 const clubsRouter = require('./routes/clubs');
+const chatRouter = require('./routes/chat');
 
 app.use('/', indexRouter);
 app.use('/clubs', clubsRouter);
+app.use('/chat', chatRouter);
 
 server.listen(3000, () => {
   console.log('Express Server is no listeing on localhost:3000..')
