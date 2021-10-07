bd7f9cca2f0a3043dfb07f79e8479ca5937d6baa
kwonsean <gownn12@naver.com>
2021-10-07 16:51:32 +0900

    feat: Add clubsRouter
    route '/clubs'
    route '/clubs/:filename'

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const server = http.createServer(app);
 
 
 const indexRouter = require('./routes/index');
+const clubsRouter = require('./routes/clubs');
 
 app.use('/', indexRouter);
+app.use('/clubs', clubsRouter);
 
 server.listen(3000, () => {
   console.log('Express Server is no listeing on localhost:3000..')
---
+++ routes/clubs.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.route('/').get((req, res, next) => {
+  return res.send('clubsRouter works!');
+});
+
+router.route('/:filename/').get((req, res) => {
+  const fileName = req.params.filename;
+  res.send(fileName);
+});
+
+module.exports = router;
